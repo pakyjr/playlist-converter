1916a732077651c5b5a59f37f18eb7729572fb73
pjr.monto@gmail.com <>
2023-12-21 15:51:12 +0100

    Implemented basic networkHandler structure
    also fixed a small bug inside the tsconfig file

--- packages/utils/src/networkHandler.ts
+++ packages/utils/src/networkHandler.ts
@@ -1,10 +1,28 @@
-import axios from 'axios'
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios'
 
 export class NetworkHandler {
 
-    constructor() { }
+  private axiosInstance: AxiosInstance;
 
-    test() {
-        console.log("hello");
+  constructor() {
+    this.axiosInstance = axios.create({
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    });
+  }
+
+  private async request(method: string, url: string, body?: any, config?: AxiosRequestConfig): Promise<AxiosResponse> {
+    try {
+      return await this.axiosInstance.request({
+        method,
+        url,
+        data: body,
+        ...config,
+      });
+    } catch (error) {
+      console.error(`NETWORK HANDLER: ${method} request to ${url} failed!`, error);
+      throw error;
     }
+  }
 }
