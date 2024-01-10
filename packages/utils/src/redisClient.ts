b0c9598f9d79932799c5e2364f316ab16cfed42e
pjr.monto@gmail.com <pjr.monto@gmail.com>
2024-01-10 22:19:08 +0100

    Implemented redis client singleton in utils

---
+++ packages/utils/src/redisClient.ts
@@ -0,0 +1,8 @@
+import { createClient } from 'redis';
+
+const client = createClient({ url: 'redis://localhost:6379' });
+client.on('error', (err) => console.log('Redis Client Error', err));
+
+client.connect();
+
+export default client;
